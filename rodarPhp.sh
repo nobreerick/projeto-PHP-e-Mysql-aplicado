7425c249a1186e475d1d0168499e165861b0839e
nobre_erick <cursos.ericknobre@gmail.com>
2025-07-16 11:45:32 -0300

    Alterado configuração do docker-compose

--- rodarPhp.sh
+++ rodarPhp.sh
@@ -4,4 +4,4 @@ echo "iniciando container PHP"
 docker compose -f docker-compose.yml down
 docker compose -f docker-compose.yml build --no-cache && \
 docker compose -f docker-compose.yml up -d
-docker exec -it projeto-inicial-cursophpemysql-php bash
+docker exec -it php_container bash
